89ae3656bc147b98aee7115b8f79c30b6698615c
EricssonWilli <ericsson.willi@gmail.com>
2025-02-04 13:28:33 +0200

    Added alternative gagefigure

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ OUTPUT_DIRECTORY=Path("./figures")
 
 
 def size_percentages_with_stats():
-    df=pd.concat([pd.json_normalize(x["data"]).assign(year=x["year"]) for index,x in pd.read_json(INPUT_DIRECTORY/"size.json").iterrows()])
+    df=pd.concat([pd.json_normalize(x["data"]).assign(year=x["year"]) for index,x in pd.read_json(INPUT_DIRECTORY/"size.json").iterrows()],ignore_index=True)
     for i in range(1,4):
         df=df.drop(f"rationale{i}",axis=1)
     a = df.groupby("year").agg(["mean","sem"])
@@ -71,5 +71,66 @@ def size_percentages_with_stats():
     plt.savefig(OUTPUT_DIRECTORY/"size_percentages_with_stats_new.pdf")
 
 
+def alternate_gagefigure():
+    import json
+    pre_search_string = "Pre-questionnaire"
+    post_search_string = "Self-estimates"
+
+    with open(INPUT_DIRECTORY/"eerik_2021_2024.json",'r') as f:
+        jsonobj = json.load(f)
+
+    dataframes=[]
+    # find the correct exercises
+    for year,exercises in jsonobj.items():
+        print("Going through year",year)
+        for exercise_id,submissions in exercises.items():
+            if len(submissions)==0:
+                continue
+            if pre_search_string in submissions[0]["Exercise"]:
+                print("pre exercise for year",year,"found, submission count",len(submissions))
+                dataframes+=[pd.json_normalize(submissions)[[f"row{i}" for i in range(1,21)]].astype(np.float64).assign(year=int(year)).assign(prepost="pre")]
+            elif post_search_string in submissions[0]["Exercise"]:
+                print("post exercise for year",year,"found, submission count",len(submissions))
+                dataframes+=[pd.json_normalize(submissions)[[f"row{i}" for i in range(1,21)]].astype(np.float64).assign(year=int(year)).assign(prepost="post")]
+
+    df = pd.concat(dataframes,ignore_index=True)
+    # print(df)
+    # print(df.dtypes)
+    # return
+    # dataframe now correct format and ready for further working
+
+    # category order
+    hardest_categories=df.drop(["prepost","year"],axis=1).agg("mean").sort_values()
+    print("Categories from (averaged self reflects) hardest to easiest (combined pre/post)")
+    print(hardest_categories)
+
+    ordered_categories = np.array(hardest_categories.index)
+
+
+
+    aggregate_values = df.groupby(["prepost","year"],dropna=False).agg(["mean","sem"])
+
+    ordered_aggregate_values = aggregate_values[ordered_categories]
+    print(ordered_aggregate_values)
+
+    fig,ax = plt.subplots(layout="constrained")
+    print(ordered_aggregate_values.keys())
+    # return
+    years=[2021,2022,2023,2024]
+    for prepost,colormap in zip(["pre","post"],[plt.get_cmap("Blues"),plt.get_cmap("Reds")]):
+        for year in years:
+            ax.errorbar(ordered_categories,np.array([[y[cat]["mean"] for cat in ordered_categories] for x,y in ordered_aggregate_values.iterrows() if x[0]==prepost and x[1]==year]).transpose(),label=f"{prepost} {year}",color=colormap(float(year-2020)/5))
+
+    ax.tick_params(axis="x",labelrotation=45)
+    ax.set_xlabel("Categories from all time average harderst to easiest")
+    ax.set_ylabel("Perceived difficulty")
+    ax.set_ylim([1,5])
+
+    ax.legend()
+    plt.savefig(OUTPUT_DIRECTORY/"alternate_gagefigure.pdf")
+    # print(aggregate_values)
+
+
 if __name__ == "__main__":
-    size_percentages_with_stats()
+    # size_percentages_with_stats()
+    alternate_gagefigure()
